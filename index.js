fa9b04bb76f25c4fc67fa1f6856513ef275f1400
LeandroMenoci <menoci.leandro@gmail.com>
2021-07-01 22:59:44 -0300

    Acessando Atributos privados
    Diferenças entre null e undefined
    
    Aprofundando nos conceitos de referências a um espaço de memória
    
    Get e Set
    
    Melhor encapsulamento da classe
    
    Protegendo atributos sensíveis

--- index.js
+++ index.js
@@ -16,11 +16,12 @@ contaCorrenteRicardo.depositar(500);
 
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = cliente1;
 conta2.agencia = 102;
 
-contaCorrenteRicardo.transferir(200, conta2);
+let valor = 200;
+contaCorrenteRicardo.transferir(valor, conta2);
 
-console.log(conta2)
+console.log(conta2.saldo)
 
 
