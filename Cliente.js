c8ec07fe83c421c5ae29d534b53d4e7023e1d3ef
LeandroMenoci <menoci.leandro@gmail.com>
2021-07-01 23:27:06 -0300

    Construtores e estáticos
    Construtores
    
    encapsulamento
    
    atributos "readonly"
    
    Atributos estáticos

--- Cliente.js
+++ Cliente.js
@@ -1,4 +1,14 @@
+import { ContaCorrente } from "./ContaCorrente.js";
 export class Cliente {
   nome;
-  cpf;
+  _cpf;
+
+  get cpf() {
+    return this._cpf;
+  }
+
+  constructor(nome, cpf) {
+    this.nome = nome;
+    this._cpf = cpf;
+  }
 }
